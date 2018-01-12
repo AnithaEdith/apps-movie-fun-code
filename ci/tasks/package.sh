395a3d3668bfa5611fdab8cb6f9acebeaa00d136
Anitha <anitha@AMB00678.local>
2018-01-12 12:55:47 +0530

    modified:   ci/tasks/package.sh

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -10,7 +10,7 @@ popd
 jar_count=`find moviefun-source/target -type f -name *.war | wc -l`
 
 if [ $jar_count -gt 1 ]; then
-  echo "More than one war found, don't know which one to deploy. Exiting"
+  echo "More than one war found, dont know which one to deploy. Exiting"
   exit 1
 fi
 
